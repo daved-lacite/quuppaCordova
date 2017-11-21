be4bb7f63342d718687c34cf27b9a7383927cb7a
daved <daved@lacitelp.com>
2017-11-21 12:00:28 -0500

    correction js

--- www/quuppaCordova.js
+++ www/quuppaCordova.js
@@ -1,20 +1,18 @@
-cordova.define("com.quuppaCordova.cordova.plugin", function(require, exports, module) {
-	var argscheck = require('cordova/argscheck'),
-	    channel = require('cordova/channel'),
-	    utils = require('cordova/utils'),
-	    exec = require('cordova/exec'),
-	    cordova = require('cordova');
+var argscheck = require('cordova/argscheck'),
+    channel = require('cordova/channel'),
+    utils = require('cordova/utils'),
+    exec = require('cordova/exec'),
+    cordova = require('cordova');
 
-	function quuppaCordova(){
-		var me = this;
-	}
+function quuppaCordova(){
+	var me = this;
+}
 
-	quuppaCordova.prototype.getData = function(successCallback, failureCallback){
-		argscheck.checkArgs('ff', 'quuppaCordova.getData', arguments);
-		exec(successCallback, failureCallback, "quuppaCordova", "getData", []);
-	}
+quuppaCordova.prototype.getData = function(successCallback, failureCallback){
+	argscheck.checkArgs('ff', 'quuppaCordova.getData', arguments);
+	exec(successCallback, failureCallback, "quuppaCordova", "getData", []);
+}
 
-	window.quuppaTagLoc = new quuppaCordova();
-	window.plugins = window.plugins || {};
-	window.plugins.quuppaTagLoc = window.quuppaTagLoc;
-});
+window.quuppaTagLoc = new quuppaCordova();
+window.plugins = window.plugins || {};
+window.plugins.quuppaTagLoc = window.quuppaTagLoc;
