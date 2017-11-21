ddc13bc0424e022229d14108e31ffd5448c848ab
daved <daved@lacitelp.com>
2017-11-21 12:27:15 -0500

    correction java

--- src/android/quuppaCordova.java
+++ src/android/quuppaCordova.java
@@ -1,10 +1,10 @@
 package com.quuppaCordova.cordova.plugin;
 
 import org.apache.cordova.CallbackContext;
-import org.apache.cordova.CordovaActivity;
 import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CordovaResourceApi;
 import org.apache.cordova.PluginResult;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaWebView;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
