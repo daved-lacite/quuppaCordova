46017257ccd8d7c1b6d83f33d30af9a2ba13356f
daved <daved@lacitelp.com>
2017-11-21 11:03:31 -0500

    adding files

---
+++ src/android/quuppaCordova.java
@@ -0,0 +1,33 @@
+package com.quuppaCordova.cordova.plugin;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaActivity;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaResourceApi;
+import org.apache.cordova.PluginResult;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class quuppaCordova extends CordovaPlugin{
+	public quuppaCordova(){}
+
+	@Override
+	public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException{
+		JSONObject send = new JSONObject();
+		send.put("start", "Sender initialized");
+		if(action.equals("getData")){
+			
+			send.put("x", 50);
+			send.put("y", 0);
+			send.put("z", 50);
+
+			callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, send));
+			return true;
+		}
+		send.put("error", "Method not found");
+
+		callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, send));
+		return false;
+	}
+}
---
+++ www/quuppaCordova.js
@@ -0,0 +1,20 @@
+cordova.define("com.quuppaCordova.cordova.plugin", function(require, exports, module) {
+	var argscheck = require('cordova/argscheck'),
+	    channel = require('cordova/channel'),
+	    utils = require('cordova/utils'),
+	    exec = require('cordova/exec'),
+	    cordova = require('cordova');
+
+	function quuppaCordova(){
+		var me = this;
+	}
+
+	quuppaCordova.prototype.getData = function(successCallback, failureCallback){
+		argscheck.checkArgs('ff', 'quuppaCordova.getData', arguments);
+		exec(successCallback, failureCallback, "quuppaCordova", "getData", []);
+	}
+
+	window.quuppaTagLoc = new quuppaCordova();
+	window.plugins = window.plugins || {};
+	window.plugins.quuppaTagLoc = window.quuppaTagLoc;
+});
