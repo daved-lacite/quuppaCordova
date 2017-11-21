e2192d7770377f5ee5e55e08e65af8d30d65e2ab
daved <daved@lacitelp.com>
2017-11-21 14:26:24 -0500

    udp code

--- src/android/quuppaCordova.java
+++ src/android/quuppaCordova.java
@@ -8,11 +8,15 @@ import org.apache.cordova.CordovaWebView;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import java.util.Random;
+import java.net.*;
 
 public class quuppaCordova extends CordovaPlugin{
-	private Random rand = new Random();
-	private int num = rand.nextInt(50);
+	private InetSocketAddress add = new InetSocketAddress(22104);
+	private DatagramChannel channel;
+    private DatagramSocket dsocket;
+    private byte[] buffer = new byte[2048];
+    private DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+    private String obj;
 
 	public quuppaCordova(){}
 
@@ -20,10 +24,7 @@ public class quuppaCordova extends CordovaPlugin{
 	public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException{
 		JSONObject send = new JSONObject();
 		if(action.equals("getData")){
-			send.put("num", num);
-			send.put("x", 50);
-			send.put("y", 0);
-			send.put("z", 50);
+			send.put("obj", obj);
 
 			callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, send));
 			return true;
@@ -34,8 +35,26 @@ public class quuppaCordova extends CordovaPlugin{
 		return false;
 	}
 
+	private void loop(){
+		try{
+			while(true){
+				dsocket.receive(packet);
+				obj = new String(buffer, 0, packet.getLength());
+			}
+		}catch(Exception e){
+            System.err.println(e);
+        }
+	}
+
 	@Override
 	public void initialize(CordovaInterface cordova, CordovaWebView webView) {
 	    super.initialize(cordova, webView);
+	    try{
+	    	channel = DatagramChannel.open();
+	    	dsocket = channel.socket();
+	    	dsocket.bind(add);
+	    }catch(Exception e){
+            System.err.println(e);
+        }
 	}
 }
