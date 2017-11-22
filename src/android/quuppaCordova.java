ab1268cc2032f3b5e5302abf010872d583c6d3db
daved <daved@lacitelp.com>
2017-11-22 07:33:34 -0500

    receiver class thread

--- src/android/quuppaCordova.java
+++ src/android/quuppaCordova.java
@@ -12,12 +12,7 @@ import java.nio.channels.DatagramChannel;
 import java.net.*;
 
 public class quuppaCordova extends CordovaPlugin{
-	private InetSocketAddress add = new InetSocketAddress(22104);
-	private DatagramChannel channel;
-    private DatagramSocket dsocket;
-    private byte[] buffer = new byte[2048];
-    private DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-    private String obj;
+	Receiver receiver = new Receiver();
 
 	public quuppaCordova(){}
 
@@ -25,7 +20,7 @@ public class quuppaCordova extends CordovaPlugin{
 	public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException{
 		JSONObject send = new JSONObject();
 		if(action.equals("getData")){
-			send.put("obj", obj);
+			send.put("obj", receiver.getInfo());
 			/*try{
 				dsocket.receive(packet);
 				obj = new String(buffer, 0, packet.getLength());
@@ -41,27 +36,41 @@ public class quuppaCordova extends CordovaPlugin{
 		return false;
 	}
 
-	private void loop(){
-		try{
-			while(true){
-				dsocket.receive(packet);
-				obj = new String(buffer, 0, packet.getLength());
-			}
-		}catch(Exception e){
-            System.err.println(e);
-        }
-	}
-
 	@Override
 	public void initialize(CordovaInterface cordova, CordovaWebView webView) {
 	    super.initialize(cordova, webView);
-	    try{
-	    	channel = DatagramChannel.open();
-	    	dsocket = channel.socket();
-	    	dsocket.bind(add);
-	    	loop();
-	    }catch(Exception e){
-            System.err.println(e);
-        }
+	    Thread tagInfo = new Thread(receiver);
+	    tagInfo.start();
+	}
+
+	private class Receiver implements Runnable{
+		private InetSocketAddress add = new InetSocketAddress(22104);
+		private DatagramChannel channel;
+	    private DatagramSocket dsocket;
+	    private byte[] buffer = new byte[2048];
+	    private DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+	    private String obj;
+	    
+	    public Receiver(){}
+
+		@Override
+		public void run(){
+			try{
+		    	channel = DatagramChannel.open();
+		    	dsocket = channel.socket();
+		    	dsocket.bind(add);
+		    	
+		    	while(true){
+					dsocket.receive(packet);
+					obj = new String(buffer, 0, packet.getLength());
+				}
+		    }catch(Exception e){
+	            System.err.println(e);
+	        }
+		}
+
+		public String getInfo(){
+			return obj;
+		}
 	}
 }
