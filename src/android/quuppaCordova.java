df7dfe4146624d42bf4e5581238820a7b1247358
daved <daved@lacitelp.com>
2018-01-22 11:39:56 -0500

    broadcatser

--- src/android/quuppaCordova.java
+++ src/android/quuppaCordova.java
@@ -9,11 +9,27 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import java.nio.channels.DatagramChannel;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.AdvertiseCallback;
+import android.bluetooth.le.AdvertiseData;
+import android.bluetooth.le.AdvertiseSettings;
+import android.bluetooth.le.BluetoothLeAdvertiser;
+
 import java.net.*;
 
 public class quuppaCordova extends CordovaPlugin{
 	private Info info = new Info();
 	private Receiver receiver = new Receiver(info);
+	private BluetoothLeAdvertiser bluetoothLeAdvertiser;
+    private byte dataPacketCounter = 0;
+    private Object dataPacketLock = new Object();
+    private String tagId;
+
+    InetSocketAddress add = new InetSocketAddress(22104);
+    DatagramSocket dsocket;
+    byte[] buffer = new byte[2048];
+    DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
 
 	public quuppaCordova(){}
 
@@ -23,6 +39,11 @@ public class quuppaCordova extends CordovaPlugin{
 		if(action.equals("getData")){
 			send.put("obj", info.getData());
 			
+			callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, send));
+			return true;
+		}else if(action.equals("getId")){
+			send.put("id", tagId);
+
 			callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, send));
 			return true;
 		}
@@ -36,6 +57,8 @@ public class quuppaCordova extends CordovaPlugin{
 	public void initialize(CordovaInterface cordova, CordovaWebView webView) {
 	    super.initialize(cordova, webView);
 	    receiver.start();
+	    startAdvertising();
+	    String tagId = Settings.Secure.getString(this.getContentResolver(), Settings.Secure.ANDROID_ID);
 	}
 
 	private class Receiver implements Runnable{
@@ -93,4 +116,82 @@ public class quuppaCordova extends CordovaPlugin{
 			this.data = val;
 		}
 	}
+
+	private boolean startAdvertising(){
+        int mode = 1;
+        int txPower = 3;
+        byte[] bytes = createBytes();
+        try{
+            BluetoothManager btManager = (BluetoothManager) getSystemService(BLUETOOTH_SERVICE);
+            BluetoothAdapter btAdapter = btManager.getAdapter();
+            if (btAdapter.isEnabled()) {
+                AdvertiseSettings advertiseSettings = new AdvertiseSettings.Builder()
+                        .setAdvertiseMode(mode)
+                        .setTxPowerLevel(txPower)
+                        .setConnectable(true)
+                        .build();
+
+                AdvertiseData advertisementData = new AdvertiseData.Builder()
+                        .setIncludeTxPowerLevel(false)
+                        .addManufacturerData(0x00C7, bytes)
+                        .build();
+
+                bluetoothLeAdvertiser = btAdapter.getBluetoothLeAdvertiser();
+                bluetoothLeAdvertiser.startAdvertising(advertiseSettings, advertisementData, dataPacketAdvCallback);
+            }
+            return true;
+        }catch (Exception e) {
+            return false;
+        }
+    }
+
+    private byte[] createBytes(){
+        byte header = (byte)(1 << 4);
+        header |= (3 << 2);
+        header |= 1;
+
+        byte[] bytes = new byte[]{
+            (byte)0x01, // Quuppa Packet ID
+            (byte)0x21, // Device Type (0x21, android)
+              	header, // Payload header
+            (byte)0x01, (byte)0x02, (byte)0x03, (byte)0x04, (byte)0x05, (byte)0x06, // Quuppa Address payload, will be replaced shortly...
+            (byte)0xb4, // checksum, calculated later
+            (byte)0x67,(byte)0xF7,(byte)0xDB,(byte)0x34,(byte)0xC4,(byte)0x03,(byte)0x8E,(byte)0x5C,(byte)0x0B,(byte)0xAA,(byte)0x97,(byte)0x30,(byte)0x56,(byte)0xE6 // DF field, 14 octets
+        };
+
+        // inject Quuppa Address into byte array
+        byte[] qAddress = createQuuppaAddress(tagId);
+        System.arraycopy(qAddress, 0, bytes, 3, 6);
+
+        // calculate CRC and inject
+        try {
+            bytes[9] = CRC8.simpleCRC(Arrays.copyOfRange(bytes, 1, 9));
+        }catch(Exception e){
+            return null;
+        }
+        return bytes;
+    }
+
+    private byte[] createQuuppaAddress(String tagID) {
+        byte[] bytes = new byte[6];
+        bytes[0] = (byte)Integer.parseInt(tagID.substring(0,2), 16);
+        bytes[1] = (byte)Integer.parseInt(tagID.substring(2,4), 16);
+        bytes[2] = (byte)Integer.parseInt(tagID.substring(4,6), 16);
+        bytes[3] = (byte)Integer.parseInt(tagID.substring(6,8), 16);
+        bytes[4] = (byte)Integer.parseInt(tagID.substring(8,10), 16);
+        bytes[5] = (byte)Integer.parseInt(tagID.substring(10,12), 16);
+        return bytes;
+    }
+
+    private final AdvertiseCallback dataPacketAdvCallback = new AdvertiseCallback() {
+        @Override
+        public void onStartSuccess(AdvertiseSettings advertiseSettings) {
+
+        }
+
+        @Override
+        public void onStartFailure(int i) {
+            final String message = "Start Status broadcast failed error code: " + i;
+        }
+    };
 }
--- www/quuppaCordova.js
+++ www/quuppaCordova.js
@@ -13,6 +13,11 @@ quuppaCordova.prototype.getData = function(successCallback, failureCallback){
 	exec(successCallback, failureCallback, "quuppaCordova", "getData", []);
 }
 
+quuppaCordova.prototype.getId = function(successCallback, failureCallback){
+	argscheck.checkArgs('ff', 'quuppaCordova.getId', arguments);
+	exec(successCallback, failureCallback, "quuppaCordova", "getId", []);
+}
+
 window.quuppaTagLoc = new quuppaCordova();
 window.plugins = window.plugins || {};
 window.plugins.quuppaTagLoc = window.quuppaTagLoc;
